e33c05ca8805302c67b9ab1516667325be60daba
tarasradio <tarasradio@mail.ru>
2021-05-05 13:33:31 +0300

    Delete unusing usings.

--- AnalyzerControlApp/AnalyzerControlGUI/Models/Cassette.cs
+++ AnalyzerControlApp/AnalyzerControlGUI/Models/Cassette.cs
@@ -1,9 +1,4 @@
 ﻿using AnalyzerDomain.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AnalyzerControlGUI.Models
 {
--- AnalyzerControlApp/AnalyzerControlGUI/ViewModels/AnalyzerControlViewModel.cs
+++ AnalyzerControlApp/AnalyzerControlGUI/ViewModels/AnalyzerControlViewModel.cs
@@ -1,11 +1,6 @@
 ﻿using AnalyzerControlGUI.Commands;
 using AnalyzerControlGUI.Models;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AnalyzerControlGUI.ViewModels
 {
