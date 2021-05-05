da2c59cdc484bdcd1a7a7ea36e93987790d73323
tarasradio <tarasradio@mail.ru>
2021-05-05 23:44:29 +0300

    Add Connection control in control window and move properties from main view model (creating analyzer...).

--- AnalyzerControlApp/AnalyzerControlGUI/ViewModels/AnalyzerControlViewModel.cs
+++ AnalyzerControlApp/AnalyzerControlGUI/ViewModels/AnalyzerControlViewModel.cs
@@ -1,6 +1,13 @@
-﻿using AnalyzerControlGUI.Commands;
+﻿using AnalyzerConfiguration;
+using AnalyzerControl;
+using AnalyzerControl.Services;
+using AnalyzerControlGUI.Commands;
 using AnalyzerControlGUI.Models;
+using AnalyzerControlGUI.Views;
+using AnalyzerService;
+using Infrastructure;
 using System.Collections.ObjectModel;
+using ConveyorCell = AnalyzerControlGUI.Models.ConveyorCell;
 
 namespace AnalyzerControlGUI.ViewModels
 {
@@ -43,7 +50,102 @@ namespace AnalyzerControlGUI.ViewModels
             }
         }
 
+        static IConfigurationProvider provider = new XmlConfigurationProvider();
+        static Analyzer analyzer = null;
+        static ConveyorService conveyor = null;
+        static AnalyzerDemoController demoController = null;
+
+        const string controllerFileName = "DemoControllerConfiguration";
+
         public AnalyzerControlViewModel()
+        {
+            InitCustomControls();
+
+            Logger.NewInfoMessageAdded += Logger_NewInfoMessageAdded;
+            Logger.NewControllerInfoMessageAdded += Logger_NewControllerInfoMessageAdded;
+            Logger.NewDemoInfoMessageAdded += Logger_NewDemoInfoMessageAdded;
+
+            tryCreateController();
+
+            UpdateConnectionState(Analyzer.Serial.IsOpen());
+        }
+
+        private void tryCreateController()
+        {
+            try {
+                analyzer = new Analyzer(provider);
+                conveyor = new ConveyorService(54);
+                demoController = new AnalyzerDemoController(provider, conveyor);
+                demoController.LoadConfiguration(controllerFileName);
+
+                Analyzer.Serial.ConnectionChanged += UpdateConnectionState;
+            } catch {
+                Logger.Info("Возникла ошибка при запуске!");
+            }
+        }
+
+        private void UpdateConnectionState(bool state)
+        {
+            if (state) {
+                ConnectionState = "Соединение установлено";
+            } else {
+                ConnectionState = "Соединение не установлено";
+            }
+        }
+
+        private void Logger_NewDemoInfoMessageAdded(string message)
+        {
+            LogText += $"{ message }";
+        }
+
+        private void Logger_NewControllerInfoMessageAdded(string message)
+        {
+            LogText += $"{ message }";
+        }
+
+        private void Logger_NewInfoMessageAdded(string message)
+        {
+            LogText += $"{ message }";
+        }
+
+        private string _logText;
+
+        public string LogText {
+            get => _logText;
+            set {
+                _logText = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private string _connectionState;
+
+        public string ConnectionState {
+            get => _connectionState;
+            set {
+                _connectionState = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private RelayCommand _connectionCommand;
+
+        public RelayCommand ConnectionCommand
+        {
+            get {
+                return _connectionCommand ??
+                   (_connectionCommand = new RelayCommand(obj =>
+                   {
+                       ConnectionViewModel viewModel = new ConnectionViewModel(Analyzer.Serial);
+                       ConnectionWindow connectionWindow = new ConnectionWindow();
+                       connectionWindow.DataContext = viewModel;
+
+                       connectionWindow.ShowDialog();
+                   }));
+            }
+        }
+
+        private void InitCustomControls()
         {
             Cassettes = new ObservableCollection<Cassette>
             {
@@ -68,6 +170,5 @@ namespace AnalyzerControlGUI.ViewModels
             ConveyorCells[6].State = ConveyorCellState.Error;
             ConveyorCells[7].State = ConveyorCellState.Processing;
         }
-
     }
 }
